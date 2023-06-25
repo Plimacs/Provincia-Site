9a82629e0bb15d525f3797f706c18d067d11693d
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2023-06-25 17:25:58 -0300

    adicionando funcionalidades e conteúdo as páguinas "Dúvidas" e "Regras".

--- src/paginas/Duvidas.jsx
+++ src/paginas/Duvidas.jsx
@@ -5,25 +5,32 @@ import './Duvidas.css';
 const Duvidas = () => {
     return(
         <div className="duvidas-container">
-            <h1 className="duvidas-title">Tire suas dúvidas sobre o Minecraft</h1>
+            <h1 className="duvidas-title">Dúvidas frequentes <br /> щ(゜ロ゜щ)</h1>
             <p className="duvidas-description">Encontre respostas para as perguntas mais comuns dos jogadores.</p>
             <img
                 className="duvidas-image"
                 src="minecraft-duvidas.png"
                 alt="Dúvidas sobre o Minecraft"
             />
+            <h2>Lista de dúvidas e respostas. _(´･ω･`)?</h2>
             <div className="duvidas-list">
                 <div className="duvida">
-                    <h2 className="duvida-title">Como construir uma casa?</h2>
-                    <p className="duvida-description">Aprenda passo a passo como construir sua casa no Minecraft.</p>
+                    <h2 className="duvida-title">Como jogar em Província?</h2>
+                    <p className="duvida-description">É bem simples, basta ter o minecraft java, conexão com a internet e seguir este passo a passo:
+                    <br /><br />1º  Verifique se você tem o minecraft java, o java e o discord instalados no seu computador.
+                    <br /><br />2º  abra seu minecraft na versão 1.20, clique em "Multijogador" e clique na opção "Adicionar servidor", na caixa "Nome do servidor:" você vai colocar "Província" e na caixa "Endereço do servidor:" você vai colocar "provinciasvr.com.br" e vai deixar o botão "pacotes de recursos" em "SIM", os pacotes de recursos são muito importantes para que você consiga jogar. quando finalizado clique em "Concluído".
+                    <br /><br />3º  Ao tentar se conectar, você vai ser desconectado e um código vai ser gerado na sua tela, copie este código, entre no nosso discord e envie este código no privado do nosso bot.
+                    <br /><br />4º  Agora você vai se conectar novamente, e vai se registrar. digite "/Register" e coloque a senha que você deseja duas vezes, exempo: "/register senha123 senha123" (obs: não utilize senhas obvias como esta).
+                    <br /><br /> Tudo pronto! Agora é so jogar e ser feliz. caso tenha algum problema contate o nosso suporte no discord.</p>
                 </div>
                 <div className="duvida">
-                    <h2 className="duvida-title">O que são os biomas?</h2>
-                    <p className="duvida-description">Descubra os diferentes biomas do Minecraft e suas características.</p>
+                    <h2 className="duvida-title">Meu computador é fraco, consigo jogar?</h2>
+                    <p className="duvida-description">Por não usarmos mods, a maioria dos processoes são feitos pelo nosso servidor, até mesmo as animações das montarias, pets, criaturas e chefões são feitas pelo nosso servidor.
+                    <br />Se seu computador rodar o mine 1.20, você vai conseguir jogar no nosso servidor.</p>
                 </div>
                 <div className="duvida">
-                    <h2 className="duvida-title">Como encontrar diamantes?</h2>
-                    <p className="duvida-description">Saiba as melhores estratégias para encontrar diamantes no jogo.</p>
+                    <h2 className="duvida-title">Como eu protejo meu terreno?</h2>
+                    <p className="duvida-description">O /warp tutorial ensina como você pode fazer isso de uma maneira bem mais prática, mas é bem simples, basta pegar uma pá de ouro na mão e clicar com o botão direito no chão marcando 2 pontos, se as marcas ficarem douradas com blocos de ouro e luzes e sinal que tudo deu certo e seu terreno está protegido, não esqueça de dar /sethome, para não perder a localização da sua casa.</p>
                 </div>
             </div>
             <div className="duvidas-footer">
--- src/paginas/Regras.jsx
+++ src/paginas/Regras.jsx
@@ -5,25 +5,50 @@ import './Regras.css';
 const Regras = () => {
     return(
         <div className="regras-container">
-            <h1 className="regras-title">Regras do Minecraft</h1>
-            <p className="regras-description">Conheça as regras essenciais para uma experiência segura e divertida.</p>
+            <h1 className="regras-title">Regras<br />(｡･_･)ﾉﾞ</h1>
+            <p className="regras-description">  Você não precisa gravar as regras do servidor, apenas tendo bom senso você segue as regras. </p>
             <img
                 className="regras-image"
                 src="minecraft-regras.png"
                 alt="Regras do Minecraft"
-            />
+            /> 
+            <h2>Pode ou não pode?  </h2> {/*colocar "_(._. )>" depois*/}
             <div className="regras-list">
                 <div className="regra">
-                    <h2 className="regra-title">Respeite os outros jogadores</h2>
-                    <p className="regra-description">Mantenha um comportamento respeitoso e evite linguagem ofensiva.</p>
+                    <h2 className="regra-title">Regra - 01</h2>
+                    <p className="regra-description"> Proibido usar as regras de má fé ou para benefício próprio, sejá para prejudicar ou beneficiar um jogadores ou staff.</p>
                 </div>
                 <div className="regra">
-                    <h2 className="regra-title">Não trapaceie</h2>
-                    <p className="regra-description">Não utilize mods ou hacks que ofereçam vantagens injustas.</p>
+                    <h2 className="regra-title">Regra - 02</h2>
+                    <p className="regra-description"> Não toleramos qualquer tipo de trapaça ou qualquer coisa que dê vantagens sobre os demais jogadores. sejam bug, programas externos ou modificações no seu jogo.</p>
                 </div>
                 <div className="regra">
-                    <h2 className="regra-title">Proteja as construções dos outros</h2>
-                    <p className="regra-description">Não destrua ou roube as construções dos outros jogadores.</p>
+                    <h2 className="regra-title">Regra - 03</h2>
+                    <p className="regra-description"> Mantenha um comportamento respeitoso e evite linguagens ofensivas. evite Spam, flood e palavras de baixo calão.</p>
+                </div>
+                <div className="regra">
+                    <h2 className="regra-title">Regra - 04</h2>
+                    <p className="regra-description"> Não toleramos desrespeito, ou importunação, independente de quem seja ou a quem seja, jogadores ou staff, todos estão sujeitos a punições.</p>
+                </div>
+                <div className="regra">
+                    <h2 className="regra-title">Regra - 05</h2>
+                    <p className="regra-description"> Proibido a comercialização de Itens do jogo em troca de conteúdo NSFW ou de cunho ilegal. outras trocas são permitidas, dentro do nosso discord ou servidor.</p>
+                </div>
+                <div className="regra">
+                    <h2 className="regra-title">Regra - 06</h2>
+                    <p className="regra-description"> Proibido qualquer ato de grifing, vandalismo ou construçoes NSFW no mapa.</p>
+                </div>
+                <div className="regra">
+                    <h2 className="regra-title">Regra - 07</h2>
+                    <p className="regra-description"> Proibido qualquer tipo de divulgação, seja de rede social, servidor ou qualquer outra coisa, sem a devida permissão.</p>
+                </div>
+                <div className="regra">
+                    <h2 className="regra-title">Regra - 08</h2>
+                    <p className="regra-description"> Proibido áudios estourados e áudios NSFW. ou audios e músicas sem a concepção dos ouvintes.</p>
+                </div>
+                <div className="regra">
+                    <h2 className="regra-title">Regra - 09</h2>
+                    <p className="regra-description"> Proibido roubar, enganar ou lezar outros jogadores.</p>
                 </div>
             </div>
             <div className="regras-footer">
