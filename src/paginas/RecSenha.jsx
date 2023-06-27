f537176ee28c3132ff785bc735d67ad17327571f
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2023-06-27 10:49:48 -0300

    Revert "Revert "funcionalidades de compra adicionadas,""
    This reverts commit c6ab6515253119bef9b6f4b34bbcb1f9550a276a.

--- src/paginas/RecSenha.jsx
+++ src/paginas/RecSenha.jsx
@@ -1,26 +1,62 @@
-import React from "react";3
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import './RecSenha.css';
-import CompRecSenha from "../components/CompRecSenha";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { useNavigate } from 'react-router-dom';
 
 const RecSenha = () => {
+    const [email, setEmail] = useState('');
+    const navigate = useNavigate();
+
+    const recoverPassword = () => {
+      if (email) {
+        showLoading();
+        firebase.auth().sendPasswordResetEmail(email)
+          .then(() => {
+            hideLoading();
+            console.log('Email enviado')
+            alert('Email enviado com sucesso');
+            navigate('/login');
+          })
+          .catch(error => {
+            hideLoading();
+            alert(getErrorMessage(error));
+          });
+      }
+    };
+  
+    const showLoading = () => {
+    };
+  
+    const hideLoading = () => {
+    };
+  
+    const getErrorMessage = (error) => {
+    };
+
     return(
-        <div className="password-recovery-container">
-            <header className="password-recovery-header">
-                <img src="logo.png" alt="Logo do Minecraft" className="logo" />
-                <h1 className="password-recovery-title">Recuperação de Senha</h1>
-            </header>
-
-            <form className="password-recovery-form">
-                <div className="input-container">
-                    <label htmlFor="nick">Nome do Jogador:</label>
-                    <input type="text" name="nick" id="nick" placeholder="Usuário" />
-                </div>
-
-                <CompRecSenha />
-              
-            </form>
+<div className="password-recovery-container">
+       <header className="password-recovery-header">
+            <img src="logo.png" alt="Logo do Minecraft" className="logo" />
+            <h1 className="password-recovery-title">Recuperação de Senha</h1>
+        </header>
+    <form className="email">
+        <div className="input-container">
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="MeuEmail@Email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
+        
+        <button type="button" onClick={recoverPassword}>Enviar e voltar à tela de Login</button>
+
+       </form>
+</div>
     );
 };
 
